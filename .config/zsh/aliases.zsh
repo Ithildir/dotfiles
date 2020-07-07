536717967d601c9c8d3e037f26ab551074521463
Andrea Di Giorgi <adigiorgi@outlook.com>
2020-07-06 17:19:51 -0700

    Make updateall cross-platform

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,3 +1,1 @@
 alias honeygitpn="git syb staging && git pn && hub browse"
-
-alias updateall="brew update && brew upgrade && brew cleanup -s && brew cask upgrade && brew doctor && brew missing && gcloud components update --quiet"
